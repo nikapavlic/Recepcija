539265ce58092fba4f2010380d6b6d0f84c23caf
Lea Holc <holc.lea@gmail.com>
2023-04-24 19:39:11 +0200

    vstavljanje podatkov o receptorjih

---
+++ receptor.sql
@@ -0,0 +1,4 @@
+insert into receptor (emso, uporabnisko_ime, geslo, ime, priimek) values ('2101001505244', 'leaholc', 'poletje01', 'Lea', 'Holc');
+insert into receptor (emso, uporabnisko_ime, geslo, ime, priimek) values ('0307001505163', 'nikapavlic', 'pomlad02', 'Nika', 'Pavlič');
+insert into receptor (emso, uporabnisko_ime, geslo, ime, priimek) values ('1003000505067', 'spelabernardic', 'zima03', 'Špela', 'Bernardič');
+insert into receptor (emso, uporabnisko_ime, geslo, ime, priimek) values ('0123456789000', 'receptor', 'receptor', 'Receptor', 'Receptor');
