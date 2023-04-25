34632328a92ac5cb9bc1914239b9966b3f0c1ab5
Nika Pavlič <nika.pavlic775@gmail.com>
2023-04-25 14:38:30 +0200

    uvoz

---
+++ parcele.sql
@@ -0,0 +1,11 @@
+INSERT into parcela (st_gostov) values (2);
+INSERT INTO parcela (st_gostov) values (3);
+INSERT INTO parcela (st_gostov) values (4);
+
+INSERT Into parcela (st_gostov) values(5);
+
+INSERT INTO parcela (st_gostov) values(6);
+
+INSERT INTO parcela (st_gostov) values(7);
+
+INSERT INTO parcela (st_gostov) values(8)
