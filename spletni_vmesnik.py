8555b90ad26abc7690bf3462bc74a76d94e96252
Nika Pavlič <nika.pavlic775@gmail.com>
2023-05-08 12:44:51 +0200

    prijava za goste

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -3,6 +3,7 @@
 
 # uvozimo bottle.py
 from bottleext import *
+from database import *
 
 # uvozimo ustrezne podatke za povezavo
 import data.auth as auth
@@ -23,51 +24,85 @@ DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
 @get('/')
 def index():
-    return 'Začetna stran'
+    return template('zacetna_stran.html')
+    #return 'Začetna stran'
 
 # @get('/')
 # def index():
 #     redirect(url('izbira_uporabnika'))
 
-@get('/receptorji')
-def receptorji():
-    cur.execute("""
-        SELECT emso, ime, priimek
-        FROM receptor
-    """)
-    return template('receptorji.html', receptorji=cur)
+# @get('/receptorji')
+# def receptorji():
+#     cur.execute("""
+#         SELECT emso, ime, priimek
+#         FROM receptor
+#     """)
+#     return template('receptorji.html', receptorji=cur)
 
 # Način prijave:
-@get('/izbira_uporabnika')
-def izbira_uporabnika():
-    return template('izbira_uporabnika.html', izbira_uporabnika=izbira_uporabnika)
+# @get('/izbira_uporabnika')
+# def izbira_uporabnika():
+#     return template('izbira_uporabnika.html', izbira_uporabnika=izbira_uporabnika)
 
 @get('/receptor/prijava') 
-def prijava_get():
+def prijava_receptor_get():
     return template("receptor_prijava.html")
 
 # kot zaposleni se lahko prijavimo npr. z emšom 1 in geslom 1234
 @post('/receptor/prijava') 
-def prijava_post():
+def prijava_receptor_post():
     uporabnisko_ime = request.forms.get('uporabnisko_ime')
     geslo = request.forms.get('geslo')
     if uporabnisko_ime is None or geslo is None:
-        redirect(url('prijava_get'))
+        redirect(url('prijava_receptor_get'))
     hashBaza = None
     try: 
         cur.execute("SELECT geslo FROM receptor WHERE uporabnisko_ime = %s", [uporabnisko_ime])
         hashBaza = cur.fetchall()[0][0]
     except:
         hashBaza = None
-    # if hashBaza is None:
-    #     redirect(url('prijava_get'))
-    #     return
-    # if hashGesla(geslo) != hashBaza:
-    #     nastaviSporocilo('Nekaj je šlo narobe.') 
-    #     redirect(url('prijava_get'))
-    #     return
+    if hashBaza is None:
+        redirect(url('prijava_receptor_get'))
+        return
+    if geslo != hashBaza:
+      #  nastaviSporocilo('Nekaj je šlo narobe.') 
+        redirect(url('prijava_receptor_get'))
+        return
     #redirect(url('izbira_pregleda'))
     return 'Uspešna prijava'
+
+@get('/gost/prijava') 
+def prijava_gost_get():
+    return template("gost_prijava.html")
+
+# kot zaposleni se lahko prijavimo npr. z emšom 1 in geslom 1234
+@post('/gost/prijava') 
+def prijava_gost_post():
+    uporabnisko_ime = request.forms.get('uporabnisko_ime')
+    geslo = request.forms.get('geslo')
+    if uporabnisko_ime is None or geslo is None:
+        redirect(url('prijava_gost_get'))
+    hashBaza = None
+    try: 
+        cur.execute("SELECT geslo FROM uporabnik WHERE uporabnisko_ime = %s", [uporabnisko_ime])
+        hashBaza = cur.fetchall()[0][0]
+    except:
+        hashBaza = None
+    if hashBaza is None:
+        redirect(url('prijava_gost_get'))
+        return
+    if geslo != hashBaza:
+         #nastaviSporocilo('Nekaj je šlo narobe.') 
+         redirect(url('prijava_gost_get'))
+         return
+    #redirect(url('izbira_pregleda'))
+    return 'Uspešna gost prijava'
+
+
+
+
+
+
 
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
